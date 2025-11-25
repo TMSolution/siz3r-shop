cbfb89221a482f8e7a6cb1e2fa4d451dfc3b86bd
Robert Łoziński <robert.lozinski@gmail.com>
2025-11-25 15:59:24 +0100

    Update _app.js

--- pages/_app.js
+++ pages/_app.js
@@ -122,7 +122,7 @@ export default function App(props) {
 
         <script
           id="siz3r_plugin"
-          src="https://siz3r-prod.vercel.app/embedScript.js"
+          src="https://siz3r.com/embedScript.js"
           noreload="true"
         />
         <Siz3rContextProvider>
