f109cb828dc9dec4274e469cd4bf56e8d8759cbc
Robert Łoziński <robert.lozinski@gmail.com>
2026-07-24 14:41:06 +0200

    Update next.config.mjs

--- next.config.mjs
+++ next.config.mjs
@@ -2,9 +2,9 @@
 const nextConfig = {
   reactStrictMode: true,
   env: {
-    SIZ3R_API_KEY: "myyjz3ej2gxb8hn1zgj1aecp5i7e5afsfl3sd0q86ld",
+    SIZ3R_API_KEY: "jw2pcjgoyrm0vy7bucx73ikmf71y26hbjan0myaafk",
     SIZ3R_URL: "https://siz3r-dev.vercel.app",
-    SIZ3R_API_KEY_ID: "eix292dnhoff0n5w5fa3h",
+    SIZ3R_API_KEY_ID: "iovb457s67ix8286hd0vg",
   },
 };
 
